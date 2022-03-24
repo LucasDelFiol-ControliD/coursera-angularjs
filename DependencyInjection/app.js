04af4b78efab5d6b00735a6ba8ab970e0181d79b
Del Fiol <lucas.delfiol@controlid.com.br>
2022-03-23 22:38:04 -0300

    Atualizando repositório...

---
+++ DependencyInjection/app.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular.module('DIApp', []).controller('DIController', DIController);
+
+    function DIController ($scope, $filter) {
+        $scope.name = "";
+        
+        // Transforma as letras minusculas de uma string de caracteres em letras maiusculas
+        $scope.upper = function() {
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        };
+    }
+})();
--- NameCalculator/app.js
+++ NameCalculator/app.js
@@ -1,8 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('NameCalculator', [])
-    .controller('NameCalculatorController', function ($scope) {
+    angular.module('NameCalculator', []).controller('NameCalculatorController', function ($scope) {
         $scope.name = "";
         $scope.totalValue = 0;
 
